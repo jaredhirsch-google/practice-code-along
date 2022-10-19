b6b366a59bc214ee8b8b0a94b06ca11bc2fa21e0
jaredhirsch-google <66283177+jaredhirsch-google@users.noreply.github.com>
2022-10-19 10:41:08 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+//CODE ALONG, PART 1
+// Let’s say we wanted to write a function
+// that takes any two numbers as arguments,
+// multiplies them together, and logs them to
+// the console.
+
+//STEP 1: DECLARE & NAME THE FUNCTION
+//    What should I name this function?
+
+//STEP 2: PARAMETERS
+//    How many parameters will I need?
+//    How do I know this?
+//    What should I call them?
+
+//STEP 3: BODY
+//    What needs to happen to my parameters
+//    in order for my function to give me the result I want?
+//    Where do I want the result to show up?
+
+//STEP 4: FUNCTION CALL
+//    Hmmm...is my function multiplying numbers yet?
+//    Let’s call the function and pass through arguments as
+//    values to see if the function works.
+
+// CODE ALONG, PART 2
+//    Write a function that will do the following:
+//      - Print the message “Success!” to the console
+//      when the correct password-- “October”-- is entered.
+//      - Print “Try again!” if any other password is entered.
